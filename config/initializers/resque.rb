e769c0f2dc7544602916875d669c6f058e32f243
Jon Yu <jonathoy@umich.edu>
2018-12-10 09:20:55 -0500

    Add retry interface to web Resque web

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,10 +2,11 @@ require "resque"
 # throttles number of jobs being run in a queue at a time
 require "resque/throttler"
 require "resque-retry" # retries failed/exceptioned jobs
+require "resque-retry/server"
 require "resque/failure/redis"
 require "resque/server"
-require 'resque-scheduler'
-require 'resque/scheduler/server'
+require "resque-scheduler"
+require "resque/scheduler/server"
 
 REDIS = Redis.new(:host =>  ENV["REDIS_HOST_IP"], :port => ENV["REDIS_PORT"])
 Resque.redis = REDIS
