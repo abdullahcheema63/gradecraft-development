9d59446e88ff4ac56de43fc79a5d57d2a824af55
Jon Yu <jonathoy@umich.edu>
2018-12-19 20:32:12 -0500

    Use safe-operator vs try, clean up

--- app/exporters/submission_exporter.rb
+++ app/exporters/submission_exporter.rb
@@ -8,17 +8,17 @@ class SubmissionExporter
           submission.assignment.assignment_type.name,
           submission.assignment_id,
           submission.assignment.name,
-          submission.student.try(:email),
+          submission.student&.email,
           submission.student_id,
-          submission.group.try(:name),
+          submission.group&.name,
           find_team_name(submission),
           submission.text_comment,
           submission.created_at,
           submission.updated_at,
-          submission.grade.try(:score),
-          find_graded_by_name(submission.grade.try(:graded_by_id)),
-          submission.grade.try(:feedback),
-          submission.grade.try(:graded_at)
+          submission.grade&.score,
+          find_graded_by_name(submission.grade&.graded_by_id),
+          submission.grade&.feedback,
+          submission.grade&.graded_at
         ]
       end
     end
@@ -28,22 +28,31 @@ class SubmissionExporter
 
   def baseline_headers(course)
     [
-      "Submission ID", "#{course.assignment_term} Type",
-      "#{course.assignment_term} ID", "#{course.assignment_term} Name",
-      "#{course.student_term} Email", "#{course.student_term} ID",
-      "#{course.group_term} Name", "#{course.team_term} Name",
-      "#{course.student_term} Comment", "Created At", "Updated At", "Score",
-      "Graded By", "Grader Feedback", "Grade Last Updated"
+      "Submission ID",
+      "#{course.assignment_term} Type",
+      "#{course.assignment_term} ID",
+      "#{course.assignment_term} Name",
+      "#{course.student_term} Email",
+      "#{course.student_term} ID",
+      "#{course.group_term} Name",
+      "#{course.team_term} Name",
+      "#{course.student_term} Comment",
+      "Created At",
+      "Updated At",
+      "Score",
+      "Graded By",
+      "Grader Feedback",
+      "Grade Last Updated"
     ]
   end
 
   def find_graded_by_name(graded_by_id)
-    return nil unless !graded_by_id.nil?
+    return if graded_by_id.nil?
     User.find(graded_by_id).name
   end
 
   def find_team_name(submission)
-    return nil unless submission.student.present?
-    submission.student.team_for_course(submission.course).try(:name)
+    return unless submission.student.present?
+    submission.student.team_for_course(submission.course)&.name
   end
 end
