8db56604a87456999c568ed7b5c182339f2b779d
Jon Yu <jonathoy@umich.edu>
2018-12-20 08:58:22 -0500

    Add Rollbar config for Resque errors

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,5 @@
 require "resque"
+require "rollbar"
 # throttles number of jobs being run in a queue at a time
 require "resque/throttler"
 require "resque-retry" # retries failed/exceptioned jobs
@@ -8,19 +9,19 @@ require "resque/server"
 require "resque-scheduler"
 require "resque/scheduler/server"
 
-REDIS = Redis.new(:host =>  ENV["REDIS_HOST_IP"], :port => ENV["REDIS_PORT"])
+REDIS = Redis.new(host: ENV["REDIS_HOST_IP"], port: ENV["REDIS_PORT"])
 Resque.redis = REDIS
 
-Resque::Failure::MultipleWithRetrySuppression.classes = [Resque::Failure::Redis]
+Resque::Failure::MultipleWithRetrySuppression.classes = [Resque::Failure::Redis, Resque::Failure::Rollbar]
 Resque::Failure.backend = Resque::Failure::MultipleWithRetrySuppression
 
 # rate limits by queue
 
 # process 20 jobs/second max
-Resque.rate_limit(:pageview_event_logger, at: 20, :per => 1)
+Resque.rate_limit(:pageview_event_logger, at: 20, per: 1)
 
 # process 2 jobs/second max
-Resque.rate_limit(:login_event_logger, at: 2, :per => 1)
+Resque.rate_limit(:login_event_logger, at: 2, per: 1)
 
 # process 20 jobs/second max
-Resque.rate_limit(:predictor_event_logger, at: 20, :per => 1)
+Resque.rate_limit(:predictor_event_logger, at: 20, per: 1)
