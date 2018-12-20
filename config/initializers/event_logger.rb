d6ea709493fbc78af3ca4fc8d4d2de6313fbbcd6
Jon Yu <jonathoy@umich.edu>
2018-12-20 10:05:10 -0500

    Up retries to 10 for a happy medium

--- config/initializers/event_logger.rb
+++ config/initializers/event_logger.rb
@@ -1,6 +1,6 @@
 EventLogger.configure do |config|
-  # retries max 7 times @ 0s, 15s, 1m, 10m, 1h, 3h, 6h
-  config.backoff_strategy = [0, 15, 60, 600, 3600, 10800, 21600]
+  # retries max 10 times @ 0s, 15s, 30s, 1m, 2m, 5m, 10m, 1h, 3h, 6h intervals
+  config.backoff_strategy = [0, 15, 30, 60, 120, 300, 600, 3600, 10800, 21600]
 
   # config.backoff_strategy = [
   #   0, 15, 30, 45, 60, 90, 120, 150, 180, 240, 300, 360, 420, 540, 660, 780,
