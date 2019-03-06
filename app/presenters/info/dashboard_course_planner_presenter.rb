bc5b7c7982fc2cca9415b2ea21919be38315c830
Erik Barroso <ebarroso@umich.edu>
2019-03-06 16:08:42 -0500

    another pass at filtering tables

--- app/presenters/info/dashboard_course_planner_presenter.rb
+++ app/presenters/info/dashboard_course_planner_presenter.rb
@@ -16,12 +16,29 @@ class Info::DashboardCoursePlannerPresenter < Showtime::Presenter
     student.grades.where(course: course).instructor_modified.order_by_updated_at_date
   end
 
-  def student_grades_for_course
-    student.grades.where(course: course).instructor_modified.student_visible.order_by_updated_at_date
+  def student_grades_for_course_without_resubmissions
+    grades_for_dashboard = []
+    resubmitted_assignments = []
+    grades_for_dashboard << student.grades.where(course: course).instructor_modified.student_visible.order_by_updated_at_date
+    resubmitted_assignments << student.submissions.where(course: course).resubmitted
+
+    grades_for_dashboard = grades_for_dashboard.first
+    resubmitted_assignments = resubmitted_assignments.first
+
+    for grade in grades_for_dashboard do
+      for resubmission in resubmitted_assignments do
+        if (grade.assignment_id == resubmission.assignment_id)
+          grades_for_dashboard.delete(grade)
+        end
+      end
+    end
+    return grades_for_dashboard
   end
 
-  def student_ungraded_submissions
-    student.submissions.where(course: course).ungraded.order_by_submitted
+  def student_ungraded_or_resubmitted_submissions
+    submissions_for_dashboard = []
+    submissions_for_dashboard << student.submissions.where(course: course).ungraded + student.submissions.where(course: course).resubmitted + student.grades.where(course: course, student_visible: false)
+    submissions_for_dashboard.first
   end
 
   def assignments
