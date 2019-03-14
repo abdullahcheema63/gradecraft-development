c2347f1f6d2dd57199c20faa8711a6189e622493
Sophia A Zhou <sophiaz@umich.edu>
2019-03-14 09:27:32 -0400

    trying to add a new grandchild component for button-modal pairing

---
+++ app/assets/javascripts/vue/components/buttonModalPair.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button class="action" @click="toggleModalState">
+      <slot name="button-text"></slot>
+    </button>
+
+    <!-- <div class="overlay" :class="modalClass" @click="close"></div> -->
+
+    <modalComponent :modalState="modalState" @close="toggleModalState" class="component_container">
+    </modalComponent>
+  </div>
+</template>
+
+<script lang='coffee'>`
+module.exports = {
+  name: 'button-modal-pair',
+  components: {
+    modalComponent: () => VComponents.get('vue/components/modalComponent')
+  },
+  props: {
+    modalState: {
+      default: false,
+      type: Boolean
+    }
+  },
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+    toggleModalState(){
+      this.modalState = !this.modalState
+    }
+  },
+  computed: {
+    modalClass() {
+      if (this.modalState) {
+        return 'is-open';
+      }
+      return 'is-closed';
+    }
+  }
+}
+`</script>
