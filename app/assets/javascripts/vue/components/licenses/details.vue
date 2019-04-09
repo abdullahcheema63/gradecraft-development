027bc0f46088aea57e78ba3c719ef63360197951
Sophia A Zhou <sophiaz@umich.edu>
2019-04-09 14:36:45 -0400

    styling for license form + refactoring

--- app/assets/javascripts/vue/components/licenses/details.vue
+++ app/assets/javascripts/vue/components/licenses/details.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
+    <p>
+      With your
+      <b>licensed account,</b>
+      you can explore all GradeCraft has to offer and <b>create licensed courses.</b> <br />
+      Licensed courses have the ability to:
+    </p>
+    <ul>
+      <li>Integrate with other tools (like Canvas or Moodle)</li>
+      <li>Import or add other users (such as assistants and students)</li>
+    </ul>
+    <p>
+      Your
+      <b>{{license.license_type_name}} license</b>
+      allows you
+      <b>{{license.max_courses || "unlimited"}}</b>
+      licensed courses.
+    </p>
+    <p>Your license: </p>
+    <ul>
+      <li>
+        <b>Expires </b>
+        {{license.expires}}
+      </li>
+      <li>
+        Was created {{license.created_at}}
+      </li>
+      <li>
+        Was last updated {{license.updated_at}}
+      </li>
+    </ul>
+
     <dt>License Type</dt>
     <dd>{{license.license_type_name}}</dd>
     <dt>Expired?</dt>
