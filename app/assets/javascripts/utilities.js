8d9d11c216089e6da4f5e683dd4a92ec4dee5913
Cait Holman <caitlin.holman@gmail.com>
2019-04-15 13:29:48 -0400

    adding image button to menubar for froala

--- app/assets/javascripts/utilities.js
+++ app/assets/javascripts/utilities.js
@@ -33,15 +33,15 @@ $( "#tabs" ).tabs({
   heightMin: 200,
   toolbarButtons: [
     'bold', 'italic', 'underline', 'paragraphFormat', 'insertTable', 'formatOL', 'formatUL','align',
-    'outdent', 'indent', 'insertLink', 'undo', 'redo', 'clearFormatting', 'image', 'insertVideo', 'html'
+    'outdent', 'indent', 'insertLink', 'undo', 'redo', 'clearFormatting', 'insertImage', 'insertVideo', 'html'
   ],
   toolbarButtonsSM: [
     'bold', 'italic', 'underline', 'paragraphFormat', 'insertTable', 'formatOL', 'formatUL','align',
-    'outdent', 'indent', 'insertLink', 'undo', 'redo', 'clearFormatting', 'image', 'insertVideo', 'html'
+    'outdent', 'indent', 'insertLink', 'undo', 'redo', 'clearFormatting', 'insertImage', 'insertVideo', 'html'
   ],
   toolbarButtonsXS: ['bold', 'italic', 'underline'],
   toolbarButtonsMD: ['bold', 'italic', 'underline', 'paragraphFormat', 'insertTable', 'formatOL', 'formatUL','align',
-  'outdent', 'indent', 'insertLink', 'undo', 'redo', 'clearFormatting', 'image', 'insertVideo', 'html']
+  'outdent', 'indent', 'insertLink', 'undo', 'redo', 'clearFormatting', 'insertImage', 'insertVideo', 'html']
 })
 
 // handle 'select all' buttons, used on release grade forms
