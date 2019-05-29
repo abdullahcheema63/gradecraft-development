3438eae92f874b975ca4ebefdda3ecb6439c163a
Sophia A Zhou <sophiaz@umich.edu>
2019-05-29 14:33:07 -0400

    building a static prototype for a table of accordions

--- app/assets/javascripts/vue/components/assignmentComponent.vue
+++ app/assets/javascripts/vue/components/assignmentComponent.vue
@@ -3,10 +3,12 @@
     <template slot="heading">Assignments</template>
     <template slot="content">
       <div class="table_container">
-        <table>
+        <table class="accordion_table">
           <thead>
             <tr>
-              <th>Assignment</th>
+              <th class="assignment_title">Assignment Title</th>
+              <th class="assignment_due">Due Date</th>
+              <th class="assignment_legend"></th>
             </tr>
           </thead>
           <tbody>
@@ -18,8 +20,69 @@
                 </template>
               </accordionComponent>
             </tr>
+            <tr class="accordion">
+              <td>
+                <h3>
+                  <span></span>
+                  Test Assignment Title
+                </h3>
+              </td>
+              <td>
+                test
+              </td>
+              <td>
+                test
+              </td>
+              <div class="is-closed">
+                <div>
+                  <h5>Testing content</h5>
+                </div>
+              </div>
+            </tr>
           </tbody>
         </table>
+      </div>
+
+      <h2>Test Structure: Static Prototype</h2>
+      <div class="table_container">
+        <div class="accordion_table">
+          <div class="accordion_table-head">
+            <p>Assignment Title</p>
+            <p>Due Date</p>
+            <p> </p>
+          </div>
+          <div class="accordion_table-body">
+            <tableAccordion v-for="assignment in allAssignments">
+              <template slot="assignment_title">{{assignment.name}}</template>
+              <template slot="assignment_due">{{assignment.dueBy}}</template>
+              <template slot="assignment_content">
+                <div>
+                  <h3>Assignment Summary</h3>
+                  <p>
+                    <b>Opens on:</b>
+                    <br />
+                    {{assignment.openOn}}
+                  </p>
+                  <p>
+                    <b>Due by:</b>
+                    <br />
+                    {{assignment.dueBy}}
+                  </p>
+                  <p>
+                    <b>Accepted until:</b>
+                    <br />
+                    {{assignment.acceptUntil}}
+                  </p>
+
+                </div>
+                <div>
+                  <h3>Assignment Actions</h3>
+                  {{assignment.points}}
+                </div>
+              </template>
+            </tableAccordion>
+          </div>
+        </div>
       </div>
     </template>
   </accordionComponent>
@@ -30,6 +93,7 @@ module.exports = {
   name: 'assignment-component',
   components: {
     accordionComponent: () => VComponents.get('vue/components/accordionComponent'),
+    tableAccordion: () => VComponents.get('vue/components/tableAccordion'),
     tablePagination: () => VComponents.get('vue/components/tablePagination'),
   },
   data() {
@@ -43,12 +107,16 @@ module.exports = {
         {
           name: "Assignment1",
           points: 1000,
-          dueAt: "december 32 2021",
+          openOn: "December 33, 2020",
+          dueBy: "December 32 2021",
+          acceptUntil: "May 40, 2040"
         },
         {
           name: "Assignment2",
           points: 2000,
-          dueAt: "december 32 2222"
+          openOn: "December 33, 2020",
+          dueBy: "December 32 2222",
+          acceptUntil: "May 40, 2040"
         }
       ]
       return assignments
---
+++ app/assets/javascripts/vue/components/tableAccordion.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="accordion in_table">
+    <div class="row_heading" :class="{open:active}" @click="toggleActive">
+      <h3>
+        <span></span>
+        <slot name="assignment_title"></slot>
+      </h3>
+      <p class="date">
+        <slot name="assignment_due"></slot>
+      </p>
+    </div>
+
+    <div :class="accordionClass">
+      <div class="content">
+        <slot name="assignment_content"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script lang='coffee'>`
+module.exports = {
+  name: 'accordion-component',
+  props: ['accordion_class'],
+  data() {
+    return {
+      active: false
+    }
+  },
+  methods: {
+    toggleActive() {
+      this.active = !this.active
+    }
+  },
+  computed: {
+    accordionClass() {
+      if (this.active) {
+        return 'is-open';
+      }
+      return 'is-closed';
+    }
+  }
+}
+`</script>
