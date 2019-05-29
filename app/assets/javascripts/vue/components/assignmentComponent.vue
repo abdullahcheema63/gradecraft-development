75e23df51cb5e4ca03ce1e5d7fcf9ff52944e0dd
Erik Barroso <ebarroso@umich.edu>
2019-05-29 10:49:01 -0400

    started work on table structure

---
+++ app/assets/javascripts/vue/components/assignmentComponent.vue
@@ -0,0 +1,60 @@
+<template>
+  <accordionComponent>
+    <template slot="heading">Assignments</template>
+    <template slot="content">
+      <div class="table_container">
+        <table>
+          <thead>
+            <tr>
+              <th>Assignment</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="assignment in allAssignments">
+              <accordionComponent>
+                <template slot="heading">{{assignment.name}}</template>
+                <template slot="content">
+                  {{assignment.points}}
+                </template>
+              </accordionComponent>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </template>
+  </accordionComponent>
+</template>
+
+<script lang='coffee'>`
+module.exports = {
+  name: 'assignment-component',
+  components: {
+    accordionComponent: () => VComponents.get('vue/components/accordionComponent'),
+    tablePagination: () => VComponents.get('vue/components/tablePagination'),
+  },
+  data() {
+    return {
+      hello: "hello"
+    }
+  },
+  computed: {
+    allAssignments(){
+      var assignments = [
+        {
+          name: "Assignment1",
+          points: 1000,
+          dueAt: "december 32 2021",
+        },
+        {
+          name: "Assignment2",
+          points: 2000,
+          dueAt: "december 32 2222"
+        }
+      ]
+      return assignments
+    }
+  },
+  methods: {
+  }
+}
+`</script>
