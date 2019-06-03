5f38236b9f2ba00dd9ce4529ae12866ef6cc3f46
Aditya Manjusha Shylesh <adishy@umich.edu>
2019-06-03 12:40:29 -0400

    Added a check for generating a username from the email address only if the username is blank in services/creates_new_user/generates_usernames.rb

--- app/services/creates_new_user/generates_usernames.rb
+++ app/services/creates_new_user/generates_usernames.rb
@@ -8,16 +8,18 @@ module Services
       executed do |context|
         user = context[:user]
 
-        new_username = username_from_email(user.email)
+        if user.username.blank?
+          new_username = username_from_email(user.email) 
         
-        username_duplicates = User.where(username: new_username).length
+          username_duplicates = User.where(username: new_username).length
       
-        if username_duplicates > 0
-          changed_username_duplicates = User.where("username ~* ?", new_username.to_s + '_\d+').length
-          new_username += "_#{username_duplicates + changed_username_duplicates}"
-        end
+          if username_duplicates > 0
+            changed_username_duplicates = User.where("username ~* ?", new_username.to_s + '_\d+').length
+            new_username += "_#{username_duplicates + changed_username_duplicates}"
+          end
 
-        user.username = new_username
+          user.username = new_username
+        end
         
         if user.internal?
           user.email = email_from_username(user.username) if user.email.blank?
