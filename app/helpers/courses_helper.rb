7393f603fd2599e3f53a3fb13bae4fa7c2b6ebd7
Aditya Manjusha Shylesh <adishy@umich.edu>
2019-06-04 10:27:57 -0400

    Added available_teams in helpers/courses_helper.rb and a teams dropdown to select teams

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -18,6 +18,11 @@ module CoursesHelper
     roles
   end
 
+  def available_teams(course)
+      teams = course.teams.pluck("name")
+      return teams
+  end
+
   def bust_course_list_cache(user)
     expire_fragment current_courses_cache_key(user)
     expire_fragment archived_courses_cache_key(user)
