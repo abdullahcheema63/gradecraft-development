024988614d729e74a133bc2aefab59be4900db8a
Aditya Manjusha Shylesh <adishy@umich.edu>
2019-06-11 16:47:22 -0400

    Changed assets/javascripts/collapsible_navigation.js to store collapsed navigation states across logins and page refreshes

--- app/assets/javascripts/behaviors/collapsible_navigation.js
+++ app/assets/javascripts/behaviors/collapsible_navigation.js
@@ -1,17 +1,27 @@
-var NavigationSectionTitles = document.querySelectorAll(".collapsible_navigation_toggle")
+var NavigationSectionTitles = Array.prototype.slice.call(document.querySelectorAll(".collapsible_navigation_toggle"));
 var ShowOnLoad = [NavigationSectionTitles[0], 
-                  NavigationSectionTitles[1], 
-                  NavigationSectionTitles[5], 
-                  NavigationSectionTitles[6]]
+                  NavigationSectionTitles[0+NavigationSectionTitles.length/2]];
 
-NavigationSectionTitles.forEach(function(_Title){
-    if(ShowOnLoad.indexOf(_Title) == -1){
-        _Title.children[0].children[0].classList.add("collapsible__indicator_collapsed");
-        _Title.parentElement.children[1].classList.add("hidden") 
-    }
-    
-    _Title.addEventListener("click", function(){
-        if(_Title.parentElement.children[1].classList.contains("hidden")){
+var Uncollapsed = [0, 0 + NavigationSectionTitles.length/2];
+
+var SavedState = localStorage.getItem("uncollapsed");
+var SavedLength = localStorage.getItem("sidebarnav_length")
+
+if(SavedState == null || !SavedLength || parseInt(SavedLength) != NavigationSectionTitles.length){
+    localStorage.setItem("uncollapsed", Uncollapsed.join());
+    localStorage.setItem("sidebarnav_length", NavigationSectionTitles.length);
+}
+else{
+    Uncollapsed = SavedState.split(',').map(function(_Index) {
+        return parseInt(_Index);
+    });
+}
+
+function ApplyCollapsedStates(){
+    var Index = 0;
+
+    NavigationSectionTitles.forEach(function(_Title){
+        if(Uncollapsed.includes(Index)){
             _Title.parentElement.children[1].classList.remove("hidden") 
             _Title.children[0].children[0].classList.remove("collapsible__indicator_collapsed")
         }
@@ -19,5 +29,28 @@ NavigationSectionTitles.forEach(function(_Title){
             _Title.parentElement.children[1].classList.add("hidden")
             _Title.children[0].children[0].classList.add("collapsible__indicator_collapsed")
         }
+        ++Index;
+    });
+}
+
+ApplyCollapsedStates();
+
+NavigationSectionTitles.forEach(function(_Title){
+    _Title.addEventListener("click", function(){
+        var Index = NavigationSectionTitles.indexOf(_Title);
+        var NextIndex = (Index + NavigationSectionTitles.length/2) % NavigationSectionTitles.length;
+
+        if(Uncollapsed.includes(Index)){
+            Uncollapsed.splice(Uncollapsed.indexOf(Index), 1);
+            Uncollapsed.splice(Uncollapsed.indexOf(NextIndex), 1);
+        }
+        else{
+            Uncollapsed.push(Index);
+            Uncollapsed.push(NextIndex);
+        }
+
+        localStorage.setItem("uncollapsed", Uncollapsed.join());
+
+        ApplyCollapsedStates();
     });
 })
