448294d56bdad3d354d30af19db7922e3af4b61d
Aditya Manjusha Shylesh <adishy@umich.edu>
2019-06-17 16:40:11 -0400

    Added helpers/info_helper.rb for performing checks for the empty state in the assignment analytics view (views/info/per_assign.html.haml)

---
+++ app/helpers/info_helper.rb
@@ -0,0 +1,10 @@
+module InfoHelper
+    def any_one_assignment_has_two_submissions?(assignment_types)
+        assignment_types.each do |assignment_type|
+            if assignment_type.assignments.select {|a| a.grades.student_visible.length > 1}.present?
+                return true
+            end
+        end
+        return false
+    end
+end
