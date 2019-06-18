8c200bc74f31e2e005a823a21c0348ad15ea83df
Aditya Manjusha Shylesh <adishy@umich.edu>
2019-06-18 15:59:51 -0400

    Removed underscore for generated username

--- app/services/creates_new_user/generates_usernames.rb
+++ app/services/creates_new_user/generates_usernames.rb
@@ -30,7 +30,7 @@ module Services
         username_duplicates = User.where(username: new_username).length
       
         if username_duplicates > 0
-          changed_username_duplicates = User.where("username ~* ?", new_username.to_s + '_\d+').length
+          changed_username_duplicates = User.where("username ~* ?", new_username.to_s + '\d+').length
           new_username += "_#{username_duplicates + changed_username_duplicates}"
         end
 
