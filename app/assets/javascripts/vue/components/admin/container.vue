538bd15fff8e0070aa6c39cb03a0ca28c23abf3c
Erik Barroso <ebarroso@umich.edu>
2019-06-26 15:47:41 -0400

    moves new activity into component from admin container

--- app/assets/javascripts/vue/components/admin/container.vue
+++ app/assets/javascripts/vue/components/admin/container.vue
@@ -8,161 +8,7 @@
       </guideMessage>
 
       <newActivity></newActivity>
-
-      <h2>New Activity</h2>
-      <p>In the past 10 days: </p>
-      <div class="flex-3">
-        <div class="bg-blue_2">
-          <h4>New User Accounts</h4>
-          <p class="summary_data">
-            <span class="lining_figures">{{newTrialUsers.length}}</span>
-            free trial accounts
-          </p>
-          <p class="summary_data">
-            <span class="lining_figures">{{newLicenseUsers.length}}</span>
-            licensed accounts
-          </p>
-        </div>
-        <div class="bg-blue_2">
-          <h4>User Licenses</h4>
-          <p class="summary_data">
-            <span class="lining_figures">4</span>
-            new licenses
-          </p>
-          <p class="summary_data">
-            <span class="lining_figures">5</span>
-            renewed licenses
-          </p>
-        </div>
-        <div class="bg-blue_2">
-          <h4>New Courses</h4>
-          <p class="summary_data column-2">
-            <span class="lining_figures">{{allNewCourses.length}}</span>
-            total
-          </p>
-          <p class="summary_data column-2">
-            <span class="lining_figures">{{newTrialCourses.length}}</span>
-            free trial courses
-          </p>
-          <p class="summary_data column-2">
-            <span class="lining_figures">Uh</span>
-            copied
-          </p>
-          <p class="summary_data column-2">
-            <span class="lining_figures">{{newLicenseCourses.length}}</span>
-            licensed courses
-          </p>
-        </div>
-      </div>
     </div>
-
-    <accordionComponent>
-      <template slot="heading">New courses</template>
-      <template slot="content">
-        <p>Courses created in the past 10 days</p>
-        <div class="table_container">
-          <table>
-            <thead>
-              <tr>
-                <th>Course # </th>
-                <th>Course Name </th>
-                <th>Licensed </th>
-                <th>Active </th>
-                <th>Published </th>
-                <th>Copied </th>
-                <th>Instructor(s) </th>
-                <th>Semester </th>
-                <th>Year </th>
-                <th>Created </th>
-                <th>Actions </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="course in allNewCourses" :key="course.id">
-                <td><a href="#">{{course.id}}</a> </td>
-                <td><a href="#">{{course.name}}</a> </td>
-                <td><span :class="{checked: course.licensed}"></span> </td>
-                <td><span :class="{checked: course.active}"></span> </td>
-                <td><span :class="{checked: course.published}"></span> </td>
-                <td><span :class="{checked: course.copied}"></span> </td>
-                <td>
-                  <ul>
-                    <li v-for="instructor in course.instructors" :key="instructor.id">
-                      <a :href="instructor.url">{{instructor.text}}</a>
-                    </li>
-                  </ul>
-                </td>
-                <td>{{course.term}}</td>
-                <td>{{course.year}}</td>
-                <td>{{course.created}}</td>
-                <td>
-                  <buttonDropdown>
-                    <template slot="button_text">Options</template>
-                    <template slot="content">
-                      <ul>
-                        <li><a :href="course.editURL">Edit</a> </li>
-                        <li><a :href="course.copyURL">Copy</a> </li>
-                        <li><a :href="course.copyStudentsURL">Copy + Students</a> </li>
-                        <li><a>Delete (needs API link made)</a> </li>
-                      </ul>
-                    </template>
-                  </buttonDropdown>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <tablePagination :items="allNewCourses" @paginate="paginateItems"></tablePagination>
-
-        <a class="button action next" href="courses/new">Add a new course</a>
-      </template>
-    </accordionComponent>
-
-    <accordionComponent>
-      <template slot="heading">License expiration</template>
-      <template slot="content">
-        <p>User accounts that will expire within 30 days from today</p>
-        <div class="table_container">
-          <table>
-            <thead>
-              <tr>
-                <th>First Name </th>
-                <th>Last Name </th>
-                <th>License Expiration </th>
-                <th>Payment Method </th>
-                <th># Active Courses </th>
-                <th>Actions </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="instructor in expiringLicenseInstructors" :key="instructor.id">
-                <td><a href="#">{{instructor.firstName}}</a> </td>
-                <td><a href="#">{{instructor.lastName}}</a> </td>
-                <td>{{instructor.licenseExpires}} </td>
-                <td>{{instructor.paymentMethod}} </td>
-                <td>{{instructor.activeCoursesNumber}} </td>
-                <td>
-                  <div class="button-container">
-                    <a class="button secondary" :href="'mailto:' + instructor.email" >Send email</a>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="table_pagination">
-          <p>
-            Results: <span class="displayed">3</span> of <span class="total">100</span>
-          </p>
-          <div>
-            <button class="table_prev disabled"></button>
-            <button class="active">1</button>
-            <button>2</button>
-            <button class="table_next"></button>
-          </div>
-        </div>
-      </template>
-    </accordionComponent>
 
     <tabContainer>
       <template slot="tabBarNav">
@@ -221,9 +67,6 @@ module.exports = {
   },
   data() {
     return {
-      toggled: true,
-      currentPageItemMin: 0,
-      currentPageItemMax: 10,
       tabBarOption: ["Courses", "Instructors", "Users", "Institutions", "Utilities"],
       tabSection: ["Courses"],
       courseToLicense: "",
@@ -231,14 +74,14 @@ module.exports = {
         id: "",
         name: "",
         number: "",
-        role: "Instructor",
+        licensed: false,
+        role: "admin",
         term: {
           name: "",
           year: "",
           start: "",
           end: ""
         },
-        licensed: false
       },
       newCourseErrors: [],
     }
@@ -250,64 +93,9 @@ module.exports = {
     getUserFirstName(){
       return this.$store.getters.userFirstName;
     },
-    expiringLicenseInstructors(){
-      var allInstructors = this.allInstructors;
-      return allInstructors.filter(this.filterExpiringInstructors)
-    },
-    allCourses(){
-      return this.$store.state.allCourses;
-    },
-    allUsers(){
-      return this.$store.state.allUsers;
-    },
-    allInstructors(){
-      return this.$store.state.allInstructors
-    },
-    allNewCourses(){
-      return this.filterNewCourses(this.allCourses)
-    },
-    newTrialCourses(){
-      var allNewCourses = this.allNewCourses;
-      return allNewCourses.filter( course => {
-        if( course.licensed === false){return course}
-        return false
-        })
-    },
-    newLicenseCourses(){
-      var allNewCourses = this.allNewCourses;
-      return allNewCourses.filter( course => {
-        if( course.licensed === true){return course}
-        return false
-        })
-    },
-    allNewUsers(){
-      return this.filterNewUsers(this.allUsers)
-    },
-    newTrialUsers(){
-      var allNewUsers = this.allNewUsers;
-      return allNewUsers.filter( user => {
-        if( user.license === "trial" ){return user}
-        return false
-      })
-    },
-    newLicenseUsers(){
-      var allNewUsers = this.allNewUsers;
-      return allNewUsers.filter( user => {
-        if( user.license === "licensed" ){return user}
-        return false
-      })
-    },
   },
 
   methods: {
-    filterNewCourses(allCourses){
-      var tenDaysAgo = new Date();
-      tenDaysAgo.setDate(tenDaysAgo.getDate() - 100);
-      return allCourses.filter( course => {
-        if( course.created < tenDaysAgo ){return false}
-        return course
-      })
-    },
     addCourse(){
       var response = this.formResponse[0];
       if( response == "Create a new course"){
@@ -331,41 +119,6 @@ module.exports = {
       }
       return this.newCourseErrors
     },
-    filterByLicensedAccount(user) {
-      if (this.showLicensedAccounts && this.showFreeAccounts) {
-        return user
-      } else if (this.showLicensedAccounts && !user.licensedAccount) {
-        return false
-      } else if (this.showFreeAccounts && user.licensedAccount) {
-        return false
-      }
-      return user
-    },
-    filterExpiringInstructors(instructor){
-      var now = new Date();
-      var expirationMax = now.setDate(now.getDate() + 30);
-      var formattedDate = new Date(expirationMax);
-      var formattedInstructorExpiration = new Date(instructor.licenseExpires);
-      if (!instructor.licenseExpires) {
-        return false
-      } else if (new Date(instructor.licenseExpires) >= formattedDate) {
-        return false
-      }
-      instructor.licenseExpires = formattedInstructorExpiration
-      return instructor
-    },
-    filterNewUsers(allUsers){
-      var tenDaysAgo = new Date();
-      tenDaysAgo.setDate(tenDaysAgo.getDate() - 100);
-      return allUsers.filter( user => {
-        if( user.createdAt < tenDaysAgo ){ return false }
-        return user
-      })
-    },
-    paginateItems(itemRange){
-      this.currentPageItemMin = itemRange.min - 1;
-      this.currentPageItemMax = itemRange.max;
-    }
   }
 }
 `</script>
--- app/assets/javascripts/vue/components/admin/newActivity.vue
+++ app/assets/javascripts/vue/components/admin/newActivity.vue
@@ -40,7 +40,100 @@
         </p>
       </div>
     </div>
-  </div>
+    <accordionComponent>
+      <template slot="heading">New courses</template>
+      <template slot="content">
+        <p>Courses created in the past 10 days</p>
+        <div class="table_container">
+          <table>
+            <thead>
+              <tr>
+                <th>Course # </th>
+                <th>Course Name </th>
+                <th>Licensed </th>
+                <th>Active </th>
+                <th>Published </th>
+                <th>Copied </th>
+                <th>Instructor(s) </th>
+                <th>Semester </th>
+                <th>Year </th>
+                <th>Created </th>
+                <th>Actions </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="course in allNewCourses" :key="course.id">
+                <td><a href="#">{{course.id}}</a> </td>
+                <td><a href="#">{{course.name}}</a> </td>
+                <td><span :class="{checked: course.licensed}"></span> </td>
+                <td><span :class="{checked: course.active}"></span> </td>
+                <td><span :class="{checked: course.published}"></span> </td>
+                <td><span :class="{checked: course.copied}"></span> </td>
+                <td>
+                  <ul>
+                    <li v-for="instructor in course.instructors" :key="instructor.id">
+                      <a :href="instructor.url">{{instructor.text}}</a>
+                    </li>
+                  </ul>
+                </td>
+                <td>{{course.term}}</td>
+                <td>{{course.year}}</td>
+                <td>{{course.created}}</td>
+                <td>
+                  <buttonDropdown>
+                    <template slot="button_text">Options</template>
+                    <template slot="content">
+                      <ul>
+                        <li><a :href="course.editURL">Edit</a> </li>
+                        <li><a :href="course.copyURL">Copy</a> </li>
+                        <li><a :href="course.copyStudentsURL">Copy + Students</a> </li>
+                        <li><a>Delete (needs API link made)</a> </li>
+                      </ul>
+                    </template>
+                  </buttonDropdown>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <tablePagination :items="allNewCourses" @paginate="paginateItems"></tablePagination>
+        <a class="button action next" href="courses/new">Add a new course</a>
+      </template>
+    </accordionComponent>
+    <accordionComponent>
+      <template slot="heading">License expiration</template>
+      <template slot="content">
+        <p>User accounts that will expire within 30 days from today</p>
+        <div class="table_container" v-if="expiringLicenseInstructors.length">
+          <table>
+            <thead>
+              <tr>
+                <th>First Name </th>
+                <th>Last Name </th>
+                <th>License Expiration </th>
+                <th>Payment Method </th>
+                <th># Active Courses </th>
+                <th>Actions </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="instructor in expiringLicenseInstructors" :key="instructor.id">
+                <td><a href="#">{{instructor.firstName}}</a> </td>
+                <td><a href="#">{{instructor.lastName}}</a> </td>
+                <td>{{instructor.licenseExpires}} </td>
+                <td>{{instructor.paymentMethod}} </td>
+                <td>{{instructor.activeCoursesNumber}} </td>
+                <td>
+                  <div class="button-container">
+                    <a class="button secondary" :href="'mailto:' + instructor.email" >Send email</a>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </template>
+    </accordionComponent>
   </div>
 </template>
 
