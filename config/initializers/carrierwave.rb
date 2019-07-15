a1aa6e08029daa62d6406075fc430b00c5e73d75
Aditya Manjusha Shylesh <adishy@umich.edu>
2019-07-15 15:21:48 -0400

    Changed config/intializers/carrierwave.rb to upload files to default directory

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,6 @@ CarrierWave.configure do |config|
   # Use local storage if in development or test
   if Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
-      config.root = "/s3mnt/"
       config.storage = :file
     end
   end
@@ -11,7 +10,6 @@ CarrierWave.configure do |config|
   # Use AWS storage if in production
   if Rails.env.production?
    CarrierWave.configure do |config|
-     config.root = "/s3mnt/"
      config.storage = :file
    end
   end
