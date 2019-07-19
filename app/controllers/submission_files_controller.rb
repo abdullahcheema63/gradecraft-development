bb87b43fcd73ece1d93214fa13247aa1def73675
Aditya Manjusha Shylesh <adishy@umich.edu>
2019-07-19 10:45:18 -0400

    Changed filepath used in controllers/submission_files_controller.rb for downloading submission

--- app/controllers/submission_files_controller.rb
+++ app/controllers/submission_files_controller.rb
@@ -7,7 +7,7 @@ class SubmissionFilesController < ApplicationController
     # let's use the object_stream here because there's no reason to hit S3 twice
     #if presenter.submission_file_streamable?
       # rubocop:disable AndOr
-    send_file presenter.submission_file.file.file.file, x_sendfile: true
+    send_file "#{Rails.root}#{presenter.submission_file.file.to_s}", x_sendfile: true
 
     #send_data(*presenter.send_data_options) and return
     #else
