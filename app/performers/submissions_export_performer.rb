3432bd9938b02a889459aa6cca15cbba4a3a4baa
Erik Barroso <ebarroso@umich.edu>
2019-09-05 14:39:13 -0400

    fixes source file path for a submission file attachment for submission exports

--- app/performers/submissions_export_performer.rb
+++ app/performers/submissions_export_performer.rb
@@ -88,7 +88,7 @@ class SubmissionsExportPerformer < ResqueJob::Performer
 
   def write_submission_binary_file(submitter, submission_file, index)
     destination_file_path = submission_binary_file_path(submitter, submission_file, index)
-    source_file_path = "#{Rails.root}/#{submission_file.file.to_s}"
+    source_file_path = "#{Rails.root}#{submission_file.file.to_s}"
     FileUtils.cp(source_file_path, destination_file_path)
   end
 
