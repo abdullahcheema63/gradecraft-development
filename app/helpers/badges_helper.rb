0839f4ab6e613a98b18127f4842b0c734d0faac0
Aditya Manjusha Shylesh <adishy@umich.edu>
2019-10-04 16:32:04 -0400

    Added earned_badges_helper.rb, Changed helpers/badges_helper.rb to change displayed auto-awarded feedback to the time zone of the user

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -11,7 +11,6 @@ module BadgesHelper
     end
 
     def is_auto_awarded(badge_feedback)
-        puts "Time zone: #{current_user.time_zone}"
         badge_feedback = badge_feedback.split("Auto-awarded on ")
 
         if badge_feedback.length == 2 && badge_feedback[0].length == 0 && is_formatted_date_time(badge_feedback[1])
@@ -21,7 +20,10 @@ module BadgesHelper
         return false
     end
 
-    def get_auto_awarded_in_user_timezone
-        "Auto-awarded on #{DateTime.now.in_time_zone(current_user.time_zone)}"
+    def get_auto_awarded_in_user_timezone(badge_feedback)
+        badge_feedback_value = badge_feedback.split("Auto-awarded on ")[1]
+        badge_feedback_date_time = DateTime.strptime(badge_feedback_value, "%A, %B %d, %Y, %l:%M%p %:z")
+
+        "Auto-awarded on #{badge_feedback_date_time.in_time_zone(current_user.time_zone)}"
     end
 end
