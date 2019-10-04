7e2b0e478b1ab45cd2729b4e2c5210263c3652e8
Aditya Manjusha Shylesh <adishy@umich.edu>
2019-10-04 15:31:06 -0400

    Added helpers/badges_helpers.rb to show user timezone for auto awarded badge feedback, Changed views/badges/_all_students_earned_badges_table.haml to show user timezone

---
+++ app/helpers/badges_helper.rb
@@ -0,0 +1,27 @@
+module BadgesHelper
+    def is_formatted_date_time(badge_feedback_value)
+        begin
+            badge_feedback_date_time = DateTime.strptime(badge_feedback_value, "%A, %B %d, %Y, %l:%M%p %:z")
+            return badge_feedback_value.length == "#{badge_feedback_date_time}".length
+        rescue => exception
+            return false
+        end
+
+        return false
+    end
+
+    def is_auto_awarded(badge_feedback)
+        puts "Time zone: #{current_user.time_zone}"
+        badge_feedback = badge_feedback.split("Auto-awarded on ")
+
+        if badge_feedback.length == 2 && badge_feedback[0].length == 0 && is_formatted_date_time(badge_feedback[1])
+            return true
+        end
+
+        return false
+    end
+
+    def get_auto_awarded_in_user_timezone
+        "Auto-awarded on #{DateTime.now.in_time_zone(current_user.time_zone)}"
+    end
+end
