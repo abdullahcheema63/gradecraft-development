c1f9a78c0c49a1684dae25ae9a1acff9cdeea0a4
Erik Barroso <ebarroso@umich.edu>
2020-01-02 18:15:04 -0500

    adds challenge scores that contribute to students grade to the current nightly score recalculator job

--- app/jobs/score_recalculator_job.rb
+++ app/jobs/score_recalculator_job.rb
@@ -3,7 +3,9 @@ class ScoreRecalculatorJob
   sidekiq_options queue: 'high'
 
   def perform(student_id, course_id)
-    student = User.find(student_id)
-    student.update_course_score_and_level course_id
+    if student_id
+      student = User.find(student_id)
+      student.update_course_score_and_level course_id
+    end
   end
 end
