26f637ac707383c591c6cd7563adec70b810548f
Erik Barroso <ebarroso@umich.edu>
2020-01-13 11:44:10 -0500

    adds conditional check to vue store to see if payment card can be removed

--- app/assets/javascripts/vue/vue-store.js
+++ app/assets/javascripts/vue/vue-store.js
@@ -325,8 +325,13 @@ const store = new Vuex.Store({
           commit('addSuccessAlertMessage', message)
         }
       },
-      removePaymentMethod: async function({ commit }, paymentMethodID){
+      removePaymentMethod: async function({ commit, state }, paymentMethodID){
         console.log("removePaymentMethod action dispatched")
+        if (state.userSubscription.payment_methods.length === 1 && state.userSubscription.courses.length >= 1){
+          let message = "You cannont remove all of your payment cards while you still have a course subscribed"
+          commit('addErrorAlertMessage', message)
+          return;
+        }
         const resp = await fetch("/api/subscriptions/remove_card", {
           method: 'POST',
           headers: requestHeaders,
