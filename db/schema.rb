3dd4a8875e48ec0064e6e2dd386f60d0947a5cc0
Erik Barroso <ebarroso@umich.edu>
2020-01-17 14:33:05 -0500

    removes merge conflict notes from schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_12_04_172404) do
-=======
 ActiveRecord::Schema.define(version: 2020_01_17_163258) do
->>>>>>> master-efs
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
